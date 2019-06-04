91f3772bc89cc132d3c9162471a6af83a8730969
masahino <masahino@gmail.com>
2019-06-04 21:36:26 +0900

    add python sample

---
+++ examples/pyls.rb
@@ -0,0 +1,35 @@
+server_command = "pyls"
+command_options = []
+
+client = LSP::Client.new(server_command, command_options)
+
+# run server and send initialize
+id = client.start_server({"rootUri"=> "file://" + File.expand_path("..", __FILE__)})
+
+puts client.wait_response(id)
+client.initialized
+
+# didOpen notification
+resp = client.didOpen({"textDocument" => LSP::Parameter::TextDocumentItem.new('examples/example.py')})
+puts resp
+
+# definition
+id = client.definition({"textDocument" => LSP::Parameter::TextDocumentIdentifier.new('examples/example.py'),
+    "position" => {"line" => 3, "character" => 4}})
+resp = client.wait_response(id)
+puts "resp of definition"
+puts resp
+
+# completion
+resp = client.completion({"textDocument" => LSP::Parameter::TextDocumentIdentifier.new('examples/example.py'),
+    "position" => {"line" => 5, "character" => 6}}) do |resp|
+  puts resp
+end
+puts resp
+
+client.shutdown
+client.stop_server
+puts "recv buffer"
+puts client.recv_buffer
+puts "request buffer"
+puts client.request_buffer
