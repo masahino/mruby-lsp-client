f276a2cb842a75d7009d232b9510102b659896d0
masahino <masahino@gmail.com>
2019-06-06 21:59:50 +0900

    add example

---
+++ examples/cquery.rb
@@ -0,0 +1,35 @@
+server_command = "cquery"
+command_options = []
+
+client = LSP::Client.new(server_command, command_options)
+
+# run server and send initialize
+id = client.start_server({"rootUri"=> "file://" + File.expand_path("..", __FILE__),
+  "initializationOptions" => {"cacheDirectory" => "/tmp/cquery/cache"}})
+resp = client.wait_response(id)
+
+client.initialized
+
+# didOpen notification
+client.didOpen({"textDocument" => LSP::Parameter::TextDocumentItem.new('examples/example.c')})
+
+# definition
+id = client.definition({"textDocument" => LSP::Parameter::TextDocumentIdentifier.new('examples/example.c'),
+    "position" => {"line" => 21, "character" => 9}})
+resp = client.wait_response(id)
+puts resp
+
+# completion
+id = client.completion({"textDocument" => LSP::Parameter::TextDocumentIdentifier.new('examples/example.c'),
+    "position" => {"line" => 0, "character" => 1}})
+resp = client.wait_response(id)
+puts resp
+
+client.shutdown
+
+client.stop_server
+puts "========== recv_buffer =========="
+puts client.recv_buffer
+puts "========== request_buffer =========="
+puts client.request_buffer
+
