af6b5da3b6d9f84610840c209be454827a253ec8
masahino <masahino@gmail.com>
2019-06-22 13:47:03 +0900

    add methods

--- mrblib/client/language_features.rb
+++ mrblib/client/language_features.rb
@@ -19,5 +19,33 @@ module LSP
     def definition(params, &block)
       send_request("textDocument/definition", params, &block)
     end
+
+    def typeDefinition(params, &block)
+      send_request("textDocument/typeDefinition", params, &block)
+    end
+
+    def implementation(params, &block)
+      send_request("textDocument/implementation", params, &block)
+    end
+
+    def references(params, &block)
+      send_request("textDocument/references", params, &block)
+    end
+
+    def documentSymbol(params, &block)
+      send_request("textDocument/documentSymbol", params, &block)
+    end
+
+    def formatting(params, &block)
+      send_request("textDocument/formatting", params, &block)
+    end
+
+    def rename(params, &block)
+      send_request("textDocument/rename", params, &block)
+    end
+
+    def foldingRange(params, &block)
+      send_request("textDocument/foldingRange", params, &block)
+    end
   end
 end
