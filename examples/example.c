c1d04f4482ad509f787c17762d6571ba0722f747
masahino <masahino@gmail.com>
2019-06-22 13:45:49 +0900

    add examples

--- examples/example.c
+++ examples/example.c
@@ -1,1 +1,16 @@
-#
+#include <stdio.h>
+
+int hoge(int x)
+{
+  printf("hoge %d\n", x);
+  return x+1;
+}
+
+int main(int argc, char *argv[])
+{
+int i;
+printf("hoge %d\n", hoge(1));
+printf("hello world\n");
+}
+
+flo
