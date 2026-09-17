6fcb6c054d53c7cf02c4c1e4207362512c173bb2
masahino <masahino@gmail.com>
2026-09-17 22:57:18 +0900

    rename

--- build_config.rb
+++ build_config.rb
@@ -2,5 +2,10 @@ MRuby::Build.new do |conf|
   toolchain :gcc
   conf.gembox 'default'
   conf.gem File.expand_path(File.dirname(__FILE__))
+  conf.gem mgem: 'mruby-json' do |g|
+    g.test_rbfiles = []
+    g.test_objs = []
+    g.skip_test = true
+  end
   conf.enable_test
 end
